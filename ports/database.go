88c209b1440efe89dcc602dd6f708cc6489123bc
Puttiwat Wanna <puttiwat@opn.ooo>
2023-01-07 19:06:12 +0700

    Return id back when creating new expense

--- ports/database.go
+++ ports/database.go
@@ -4,6 +4,6 @@ import "github.com/puttiwatWan/assessment/body"
 
 type DBOperations interface {
 	CloseConnection()
-	CreateExpense(ce body.Expense) error
+	CreateExpense(ce body.Expense) (string, error)
 	GetExpenseById(id string) (body.Expense, error)
 }
